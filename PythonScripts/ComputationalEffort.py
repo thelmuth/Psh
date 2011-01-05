607a77ff6b17a0c2d43dd5686d86530a55c38d72
Tom Helmuth <thelmuth@cs.umass.edu>
2011-01-05 14:18:38 -0500

    Some small changes.

--- PythonScripts/ComputationalEffort.py
+++ PythonScripts/ComputationalEffort.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # Set these before running:
-outputDirectory = "../GAExperiments/decimation3/ssmNONE/pop2000gen401runs1000"
+outputDirectory = "../GAExperiments/decimation3/ssmTRUNCATION/pop2000gen401runs1000"
 outputFilePrefix = "run"
 outputFileSuffix = ".txt"
 
